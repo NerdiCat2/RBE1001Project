1c8c9998c18c30486b379ce62a5ed1e4ad4d3f3e
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2022-09-01 09:28:20 -0400

    init

---
+++ VexLab1/src/headerFile.h
@@ -0,0 +1,5 @@
+// Methods::
+double distanceToDegree(double cm, double track);
+void move(double cm);
+void turnAngle(double angle, double track, double diameter);
+void bump();
---
+++ VexLab1/src/main.cpp
@@ -0,0 +1,56 @@
+
+#include "vex.h"
+#include <iostream>
+#include "headerFile.h"
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Motor1               motor         1
+// Motor10              motor         10
+// RangeFinderE         sonar         E, F
+// BumperC              bumper        C
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+using namespace std;
+using namespace vex;  
+
+int main() {
+  vexcodeInit();
+  move(20);
+  move(20);
+}
+
+// Given a target distance and wheel diameter, drive straight for the given
+// distance.
+void move(double cm) {
+  double degree = distanceToDegree(cm, 10.16);
+  Motor1.rotateFor(degree, deg, 180, dps, false);
+  Motor10.rotateFor(degree, deg, 180, dps, false);
+}
+
+// Given a target angle, wheel track, and wheel diameter, turn the BaseBot a
+// specified angle.
+void turnAngle(double angle, double track, double diameter) {
+  double cm = (track * M_PI * angle) / 360;
+  double degree = distanceToDegree(cm, track);
+  Motor1.rotateFor(degree, deg, 180, dps, false);
+  Motor10.rotateFor(-degree, deg, 180, dps, false);
+}
+
+// Bumping code
+void bump() {
+  while (true) {
+    cout << BumperC.pressing() << endl;
+    if (BumperC.pressing() == 1)
+      Motor1.spin(reverse, 120, rpm);
+    else
+      Motor1.stop();
+    vex::task::sleep(100);
+  }
+}
+
+// Changes Distance in cm to degrees
+double distanceToDegree(double cm, double track) {
+  return (cm * 360 * 5) / (track * M_PI);
+}
